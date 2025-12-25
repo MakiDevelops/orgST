08744442161f07bd8ef017e3771250d8316c6fd0
Maki <makidevhelp@gmail.com>
2025-12-24 17:18:43 -0800

    test

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+#### Information:
+terminal_name = 'orgST Terminal'
+version = 1.0
+date_edited = '2025-12-24'
+
+### Imports:
+import os, time, sys
+
+## Vars:
+dir = os.getcwd()
+border = '+---------------------------+'
+ticon = '[>] ' 
+cmd_list = ['help', 'run', 'ID', 'info', 'git', 'time']
+
+def orgfetch():
+    print("                 _____ _____  ")
+    print("                /  ___|_   _| ")
+    print("  ___  _ __ __ _\ `--.  | |   ")
+    print(" / _ \| '__/ _` |`--. \ | |   ")
+    print("| (_) | | | (_| /\__/ / | |   ")
+    print(" \___/|_|  \__, \____/  \_/   ")
+    print("            __/ |             ")
+    print("           |___/              ")
+    print(border)
+    print(f"{terminal_name} {version}.")
+    print(f"Last edited: {date_edited}")
+    
+
 def main():
-    print("Welcome to orgST")
+    print(f"{terminal_name} {version}.")
+    print(f"Last edited: {date_edited}")
+    print(dir)
+    print(border)
+
+    while True:
+        inp = input(ticon)
+        if inp in cmd_list:
+            if inp == "help":
+                print("help, run, ID, info, git, time")
+            if inp == 'info':
+                orgfetch()
+        else:
+            print("Command not found.")
 
 
 if __name__ == "__main__":
